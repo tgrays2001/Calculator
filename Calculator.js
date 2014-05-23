4be68140c524958a93bb67577fe516d2bd34d44e
tgrays <tgrays2001@gmail.com>
2014-05-22 18:29:12 -0600

    Style and Function Change
    I made the styling of the calculator more modern. i also added some
    functionality to when the buttons are pressed

---
+++ Calculator.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+		$( ".numberButton" ).click(function() {
+			console.log($(this).val());
+    	$(this).val();
+			
+			//I need to create a variable and a function that assign the values inputted to the variable. 
+			//check for a operator push
+			
+});
+});
