64a2d5e9494c3411a9a48cf4a951805388a1c438
tgrays <tgrays2001@gmail.com>
2014-06-05 18:27:41 -0600

    fixed calculator display

--- Calculator.js
+++ Calculator.js
@@ -4,15 +4,18 @@ $(document).ready(function () {
     var arr = new Array();
     var nextOp;
     var stack;
+		var arr2 = new Array();
+		var sum;
 
 
 //Creating the function that will operate the numbers//
 
     $(".numberButton").click(function () {
-        stack = $(this).val();
-        calculatorInput.val(stack);
-        arr.push(parseInt(stack));
-        console.log("1st", stack);
+        arr.push($(this).val());
+        calculatorInput.val(arr[arr.length-1]);
+        //arr.push(parseInt(stack));//
+			  console.log("1st", arr);
+			  //stack = '';//
 
     });
 	
@@ -20,18 +23,21 @@ $(document).ready(function () {
 
     $(".oppButton").click(function () {
         nextOp = $(this).val();
-        arr.push(parseInt(stack));
-        console.log("2nd", stack);
+        //arr.push(parseInt(stack))//;
+				arr2.push(nextOp);
+        console.log("2nd", arr, nextOp, arr2);
 
 //Creating the if statement that analyzes what is in the array and displays it//
         if (calculatorInput.val()) {
             calculatorInput.val(arr);
             console.log("3rd", nextOp, arr);
             if (nextOp && (arr.length >= 2)) {
-                console.log("here");
+                console.log("here",arr.length);
                 var rhs = arr.pop();
+							  console.log("4th", rhs);
                 var lhs = arr.pop();
-                switch (nextOp) {
+								console.log("5th", lhs);
+                switch (arr2[0]) {
                 case '+':
                     arr.push(lhs + rhs);
                     console.log("plus");
@@ -44,6 +50,8 @@ $(document).ready(function () {
                     break;
                 case '*':
                     arr.push(lhs * rhs);
+										sum = $(this).val;
+										console.log(arr, arr2,"multi",sum );
                     break;
                 case '+':
                     arr.push(lhs + rhs);
@@ -55,6 +63,7 @@ $(document).ready(function () {
                     break;
 
                 }
+								calculatorInput.val(arr);
                 nextOp = '';
             }
 
@@ -64,6 +73,7 @@ $(document).ready(function () {
             stack = '';
             nextOp = '';
 						arr = [];
+					  arr2 = [];
             calculatorInput.val('');
 						//calculatorInput = $("#calculatorInput");//
 					  console.log(arr,"clear")
